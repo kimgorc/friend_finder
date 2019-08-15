c9f851c0a2f2505c281f94e305247c6b07f943cb
Kim Gorcyca <kgorcyca@gmail.com>
2019-08-15 15:58:53 -0700

    finalized ability to add user into the friend array

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,23 +1,17 @@
-// ===============================================================================
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-// ===============================================================================
-
 var express = require("express");
 var router = express.Router();
 var path = require("path");
 
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
-  router.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/survey.html"));
-  });
-
-  router.get("/", function(req, res) {
+router.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
+});
+
+router.get("/home", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
+router.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+});
 
 module.exports = router;
